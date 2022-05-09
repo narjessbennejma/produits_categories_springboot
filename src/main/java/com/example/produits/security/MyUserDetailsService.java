95630e933b57695f5e5f666a6f6e3c5bedeee960
narjessbennejma <narjessbennejma02@gmail.com>
2022-05-09 22:19:37 +0100

    first commit

---
+++ src/main/java/com/example/produits/security/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.produits.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import
+org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.produits.entities.User;
+import com.example.produits.entities.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+@Autowired
+UserRepository userRepository;
+@Override
+public UserDetails loadUserByUsername(String username) throws
+UsernameNotFoundException {
+User user = userRepository.findByUsername(username);
+return new MyUserDetails(user);
+ }
+}
